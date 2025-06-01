91bc373b3cd3af79df7c5341d666fade917ba4a4
Heav <thehidesofheav@gmail.com>
2025-06-01 22:05:52 +0330

    Update cipher64.py

--- cipher64.py
+++ cipher64.py
@@ -3,7 +3,7 @@ import base64
 def caesar_cipher(text, shift):
     result = ""
     for char in text:
-    		if char.isalpha():
+    	if char.isalpha():
             ascii_offset = 65 if char.isupper() else 97
             result += chr((ord(char) - ascii_offset + shift) % 26 + ascii_offset)
         else:
